ee3e97ce519029090e4c292eab5767e66eeeb107
Chris Greening <chris@cmgresearch.com>
2023-03-02 19:40:49 +0800

    Put back placeholder

--- main.py
+++ main.py
@@ -9,14 +9,7 @@ load_dotenv()
 # configure OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-INSTRUCTIONS = """You are an AI assistant that is an expert in alcoholic beverages.
-You know about cocktails, wines, spirits and beers.
-You can provide advice on drink menus, cocktail ingredients, how to make cocktails, and anything else related to alcoholic drinks.
-If you are unable to provide an answer to a question, please respond with the phrase "I'm just a simple barman, I can't help with that."
-Please aim to be as helpful, creative, and friendly as possible in all of your responses.
-Do not use any external URLs in your answers. Do not refer to any blogs in your answers.
-Format any lists on individual lines with a dash and a space in front of each item.
-"""
+INSTRUCTIONS = """<<PUT THE PROMPT HERE>>"""
 TEMPERATURE = 0.5
 MAX_TOKENS = 500
 FREQUENCY_PENALTY = 0
